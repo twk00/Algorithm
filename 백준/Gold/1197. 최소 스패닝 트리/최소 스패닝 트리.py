76f806de60eaf3ca4c2664991513567768a99ea6
twk00 <162117127+twk00@users.noreply.github.com>
2024-03-31 22:26:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 752 ms, Memory: 47260 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -58,3 +58,48 @@ for i in graph:
         union(parent, a, b)
         result += cost
 print(result)
+
+# import sys
+
+# input = sys.stdin.readline
+
+# v, e = map(int, input().split())
+# graph = []  # graph 리스트를 빈 리스트로 초기화
+# parent = [0] * (v + 1)
+# result = 0
+
+# for k in range(e):
+#     i, j, cost = map(int, input().split())
+#     graph.append((i, j, cost))  # graph 리스트에 간선 정보를 추가
+
+# # 간선의 가중치를 기준으로 오름차순으로 정렬
+# graph.sort(key=lambda x: x[2])
+# print(graph)
+# def find(parent, x):
+#     if parent[x] == x:
+#         return x
+#     parent[x] = find(parent, parent[x])
+#     return parent[x]
+
+# def union(parent, a, b):
+#     rootA = find(parent, a)
+#     rootB = find(parent, b)
+
+#     if rootA < rootB:
+#         parent[rootB] = rootA
+#     else:
+#         parent[rootA] = rootB
+
+# # 부모 테이블상에서, 부모를 자기 자신으로 초기화
+# for i in range(1, v + 1):
+#     parent[i] = i
+
+# # Kruskal 알고리즘 수행
+# for edge in graph:
+#     a, b, cost = edge
+#     if find(parent, a) != find(parent, b):
+#         union(parent, a, b)
+#         result += cost
+
+# print(result)
+
