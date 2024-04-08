6c29dc044934b51b297482dba6916a383d064609
twk00 <162117127+twk00@users.noreply.github.com>
2024-04-08 22:57:13 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+def pibo(n):
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n]
+
+print(pibo(int(input())))
