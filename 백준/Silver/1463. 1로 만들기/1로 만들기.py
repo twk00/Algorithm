fa1ec4a3d29dda6d44e41e0d16e24bc218bd8ee4
twk00 <162117127+twk00@users.noreply.github.com>
2024-04-09 16:38:36 +0900

    [Silver III] Title: 1로 만들기, Time: 332 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+def min_operations_to_1(n):
+    dp = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    return dp[n]
+
+n = int(input())
+
+print(min_operations_to_1(n))
+
