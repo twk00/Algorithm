faae4fa3783324f116d48ed910db5bc7e8c9b16c
twk00 <162117127+twk00@users.noreply.github.com>
2024-04-10 16:12:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 3008 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+weight = []
+value = []
+wv = []
+for _ in range(n):
+    w,v = map(int, input().split())
+    weight.append(w)
+    value.append(v)
+ 
+def knap(weight, value, k):
+    n = len(weight)
+    dp = [[0] * (k+1) for _ in range(n+1)]
+
+    for i in range(1,n+1):
+        for j in range(1,k+1):
+            if weight[i-1] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight[i-1]] + value[i-1])
+
+    return dp[n][k]
+
+print(knap(weight,value,k))
