5ec725da061b06586434d9422215ada4d96bd63c
twk00 <162117127+twk00@users.noreply.github.com>
2024-09-02 15:37:41 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,26 @@
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-miro = []
-dx = (-1, 1, 0, 0)
-dy = (0, 0, -1, 1)
+n,m = map(int,input().split())
+graph=[]
+deq = deque()
+dx = (-1,1,0,0)
+dy = (0,0,-1,1)
 
 for _ in range(n):
-    miro.append(list(map(int, input().rstrip())))
+    graph.append(list(int(i) for i in input().rstrip()))
 
-
-def bfs(x, y):
-    deq = deque()
-    deq.append((x, y))
+def bfs(x,y):
+    deq.append((x,y))
     while deq:
-        x, y = deq.popleft()
+        x,y = deq.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and miro[nx][ny] == 1:
-                deq.append((nx, ny))
-                miro[nx][ny] = miro[x][y] + 1
-    return miro[n - 1][m - 1]
-
-
-print(bfs(0, 0))
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if n> nx >= 0 and m > ny >= 0 and graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                deq.append((nx,ny))
+    return graph[n-1][m-1]
+print(bfs(0,0))
