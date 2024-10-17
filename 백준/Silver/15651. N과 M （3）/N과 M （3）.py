813606a5b00f4c195efb8926a61d6dbee0a59149
twk00 <162117127+twk00@users.noreply.github.com>
2024-10-17 14:22:26 +0900

    [Silver III] Title: N과 M (3), Time: 1208 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+list = []
+
+def dfs():
+
+    if len(list) == m:
+        print(' '.join(map(str,list)))
+        return
+    
+    for i in range(1,n+1):
+        list.append(i)
+        dfs()
+        list.pop()
+
+dfs()
